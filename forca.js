fc8c970ff828056d0eef8abd5b75bb40c09b9f8c
guilherme <gwoliveira@gmail.com>
2016-03-17 22:05:39 -0300

    prova de conceito

--- forca.js
+++ forca.js
@@ -1,5 +1,23 @@
 angular.module("jogo", []).controller("forca", function($scope) {
+  $scope.palavraSortiada = {
+    dica: "Heroi milionario que tinha medo de morcegos",
+    resposta: "batman"
+  }
 
+  $scope.parcial = new Array($scope.palavraSortiada.resposta.length);
+  for (var i = 0; i < $scope.palavraSortiada.resposta.length; i++) {
+    $scope.parcial[i] = "_"
+  }
+  $scope.letra = function(key) {
+    console.log(key);
+
+    for (var i = 0; i < $scope.palavraSortiada.resposta.length; i++) {
+      if ($scope.palavraSortiada.resposta[i] == key) {
+        $scope.parcial[i] = key;
+      }
+
+    }
+  }
 
 
 });
